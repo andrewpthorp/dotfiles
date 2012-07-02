bf1d3f0498ae087411b4c8fb886074dcf6a9318b
Andrew Thorp <andrewpthorp@gmail.com>
2012-07-02 18:17:02 -0500

    added tomcat aliases

---
+++ java/tomcat.zsh
@@ -0,0 +1,3 @@
+alias tomcatstart="~/Development/java/tomcat/bin/startup.sh"
+alias tomcatstop="~/Development/java/tomcat/bin/shutdown.sh"
+alias tomcatlog="tail -f ~/Development/java/tomcat/logs/catalina.out"
