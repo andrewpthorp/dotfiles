1253387b322e8b778fc9977ecc688488c6e0d66c
Andrew Thorp <andrewpthorp@gmail.com>
2012-10-29 12:25:27 -0500

    added ability to kill all PC processes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,5 @@ alias l='ls -la'
 alias lastfm='shell-fm'
 alias reload!='. ~/.zshrc'
 alias tks='tmux kill-server'
+alias killpc='echo "Desctruction Commencing, destroying local Pure Charity processes." && sudo killall -m ruby -m unicorn -m redis -m mailcatcher -m resque'
 
