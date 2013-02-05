e6f58e99a7a51df029d1d2ac9a6e50b0a01ee26b
Andrew Thorp <andrewpthorp@gmail.com>
2013-02-04 22:39:08 -0500

    removed java/tomcat

--- java/tomcat.zsh
+++
@@ -1,3 +0,0 @@
-alias tomcatstart="~/Development/java/tomcat/bin/startup.sh"
-alias tomcatstop="~/Development/java/tomcat/bin/shutdown.sh"
-alias tomcatlog="tail -f ~/Development/java/tomcat/logs/catalina.out"
