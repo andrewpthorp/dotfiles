aedddba09524baf6eec0416b4d2f0c9bc2868308
Andrew Thorp <andrewpthorp@gmail.com>
2014-10-15 10:14:50 -0500

    alias irb to pry, add update-keys

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,5 @@ alias l='ls -la'
 alias reload!='. ~/.zshrc'
 alias killallruby='echo "DESTROYING RUBY PROCESSES!" && sudo killall -m ruby -m unicorn -m redis -m mailcatcher -m resque'
 alias tomdoc='yard --plugin tomdoc'
-
+alias update-keys="gpg --keyserver pgp.mit.edu --refresh-keys andrewpthorp@gmail.com apt@stripe.com"
+alias irb='pry'
