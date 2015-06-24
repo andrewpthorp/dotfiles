6b87f189ab014a142af24d929daf3fd55f11149d
Andrew Thorp <andrewpthorp@gmail.com>
2015-06-24 16:07:47 -0500

    add updated version of bin/mx

--- tmux/tmux.zsh
+++ tmux/tmux.zsh
@@ -1,10 +1,2 @@
-function mx() {
-  if [[ -x $DOTFILES/tmux/layouts/$1 ]]; then
-    $DOTFILES/tmux/layouts/$1
-  else
-    echo "No layout for '$1'"
-  fi
-}
-
 # Aliases
 alias tks='tmux kill-server'
