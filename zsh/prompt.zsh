d8644088f57d4460e7fcfbda17ac8a217a53ecde
Andrew Thorp <andrewpthorp@gmail.com>
2015-08-06 13:50:11 -0700

    cleanup prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -7,12 +7,6 @@ ZSH_THEME_GIT_PROMPT_SUFFIX="%F{239}) "
 ZSH_THEME_GIT_PROMPT_DIRTY="%F{196}"
 ZSH_THEME_GIT_PROMPT_CLEAN="%F{28}"
 
-# Colors vary depending on time lapsed.
-ZSH_THEME_GIT_TIME_SINCE_COMMIT_SHORT="%F{239}"
-ZSH_THEME_GIT_TIME_SHORT_COMMIT_MEDIUM="%F{239}"
-ZSH_THEME_GIT_TIME_SINCE_COMMIT_LONG="%F{239}"
-ZSH_THEME_GIT_TIME_SINCE_COMMIT_NEUTRAL="%F{239}"
-
 # get the name of the branch we are on
 function git_prompt_info() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
@@ -28,60 +22,20 @@ parse_git_dirty() {
   fi
 }
 
-# Determine the time since last commit. If branch is clean,
-# use a neutral color, otherwise colors will vary according to time.
-function git_time_since_commit() {
+function git_time_since_last_commit() {
   if git rev-parse --git-dir > /dev/null 2>&1; then
-    # Only proceed if there is actually a commit.
-    if [[ $(git log 2>&1 > /dev/null | grep -c "^fatal: bad default revision") == 0 ]]; then
-      # Get the last commit.
-      last_commit=`git log --pretty=format:'%at' -1 2> /dev/null`
-      now=`date +%s`
-      seconds_since_last_commit=$((now-last_commit))
-
-      # Totals
-      MINUTES=$((seconds_since_last_commit / 60))
-      HOURS=$((seconds_since_last_commit/3600))
-
-      # Sub-hours and sub-minutes
-      DAYS=$((seconds_since_last_commit / 86400))
-      SUB_HOURS=$((HOURS % 24))
-      SUB_MINUTES=$((MINUTES % 60))
-
-      if [[ -n $(git status -s 2> /dev/null) ]]; then
-          if [ "$MINUTES" -gt 30 ]; then
-              COLOR="$ZSH_THEME_GIT_TIME_SINCE_COMMIT_LONG"
-          elif [ "$MINUTES" -gt 10 ]; then
-              COLOR="$ZSH_THEME_GIT_TIME_SHORT_COMMIT_MEDIUM"
-          else
-              COLOR="$ZSH_THEME_GIT_TIME_SINCE_COMMIT_SHORT"
-          fi
-      else
-          COLOR="$ZSH_THEME_GIT_TIME_SINCE_COMMIT_NEUTRAL"
-      fi
-
-      if [ "$HOURS" -gt 24 ]; then
-          echo "$COLOR${DAYS}d "
-      elif [ "$MINUTES" -gt 60 ]; then
-          echo "$COLOR${HOURS}h${SUB_MINUTES}m "
-      else
-          echo "$COLOR${MINUTES}m "
-      fi
-    else
-      COLOR="$ZSH_THEME_GIT_TIME_SINCE_COMMIT_NEUTRAL"
-      echo "$COLOR "
-    fi
+    last_commit=`git log --format="format:%ad" --date=relative -1 2>/dev/null`
+    echo "${last_commit}"
   fi
 }
 
 # http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html
 export PROMPT='%F{239}%~ \
-$(git_prompt_info)\
-$(git_time_since_commit)%{$reset_color%}\
+$(git_prompt_info)%{$reset_color%}\
 %F{31}%(!.#.→)%{$reset_color%} '
 
 set_prompt () {
-  export RPROMPT=""
+  export RPROMPT="%F{239}(%F{31}$(git_time_since_last_commit)%F{239})%{$reset_color%}"
 }
 
 precmd() {
